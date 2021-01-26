e1ed4890dd23cbf69631bd98561a8b679ee1f44c
jovial-application-1943 <fatima.katrib@lau.edu>
2021-01-26 21:20:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const testVar = {}
 
-function superbowlWin = (record) => {
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin (record) {
   let result = record.find( record => record.result === "W" )
-  return result.year}
+  return result.year
+  
+}
+  
   
 function testFunc() {
 
